6f3545101dc45d22f5873dc3af96d77e96b1bd74
Samuel Andrew McIntyre <fuzzylogic@apache.org>
2005-04-25 19:28:17 +0000

    Derby-233: Fix copyright header in files in java/demo, and cleanup other IBM/Cloudscape references.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/derby/code/trunk@164629 13f79535-47bb-0310-9956-ffa450edef68

--- java/demo/nserverdemo/NsSampleClientThread.java
+++ java/demo/nserverdemo/NsSampleClientThread.java
@@ -1,9 +1,21 @@
 /*
- * (C) Copyright IBM Corp. 2003.
- *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
+
+   Derby - Class nserverdemo.NsSampleClientThread
+
+   Copyright 2003, 2004 The Apache Software Foundation or its licensors, as applicable.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
  */
 
 package nserverdemo;
@@ -54,9 +66,6 @@ public class NsSampleClientThread extends Thread {
  * It includes
  * getting a connection to the database, creating and loading of schema,
  * preparing and execution of SQL statements (insert, select, update, delete )
- * <P>
- * <I>IBM Corp. reserves the right to change, rename, or
- * remove this interface at any time.</I>
  */
 class NsSampleWork {
 
--- java/demo/simple/SimpleApp.java
+++ java/demo/simple/SimpleApp.java
@@ -1,12 +1,24 @@
-import java.sql.Connection;
-
 /*
- * (C) Copyright IBM Corp. 2001, 2004.
- *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
+
+   Derby - Class SimpleApp
+
+   Copyright 2001, 2004 The Apache Software Foundation or its licensors, as applicable.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
  */
+
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
