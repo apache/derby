92ea35f5fa1c9f93d0b287774f0e6380cbc67d08
Katherine Marsden <kmarsden@apache.org>
2005-09-16 22:25:39 +0000

    DERBY-518 Data type mismatch error for boolean to DECIMAL conversion in J2ME merge from trunk
    svn merge -r 264944:264945 https://svn.apache.org/repos/asf/db/derby/code/trunk
    svn merge -r 265042:265043 https://svn.apache.org/repos/asf/db/derby/code/trunk
    svn merge -r 279925:279926 https://svn.apache.org/repos/asf/db/derby/code/trunk
    	Contributed by Deepa Remesh
    	
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/branches/10.1@289667 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/jdbcapi/resultset.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/jdbcapi/resultset.java
@@ -35,6 +35,7 @@ import org.apache.derby.tools.ij;
 import org.apache.derbyTesting.functionTests.util.TestUtil;
 import org.apache.derbyTesting.functionTests.util.JDBCTestDisplayUtil;
 import org.apache.derby.iapi.reference.JDBC30Translation;
+import org.apache.derbyTesting.functionTests.util.BigDecimalHandler;
 
 /**
  * Test of JDBC result set and result set meta-data.
@@ -145,7 +146,7 @@ public class resultset {
 				for (int i=1;i<=colCount;i++) {
 					try {
 						System.out.println("getBigDecimal("+i+",1): "+
-													rs.getBigDecimal(i, 1));
+											BigDecimalHandler.getBigDecimalString(rs,i));
 					}
 					catch (Throwable e) {
 						System.out.println(
@@ -159,7 +160,7 @@ public class resultset {
 							System.out.println("beetle 5328 - JCC returns incorrect scale for getBigDecimal(String,int)");
 						System.out.println("getBigDecimal("+
 										columnNames[i-1]+ ",1): "+
-										rs.getBigDecimal(columnNames[i-1], 1));
+										BigDecimalHandler.getBigDecimalString(rs,columnNames[i-1],i));
 					}
 					catch (Throwable e) {
 						System.out.println(
@@ -363,12 +364,12 @@ public class resultset {
 						// this does not test the values returned, just whether it gives an exception.
 						if (i>11)
 						{
-							rs.getObject(i);
+							BigDecimalHandler.getObjectString(rs,i);
 							System.out.println("getObject("+i+") is ok");
 						}
 						else
 							System.out.println("getObject("+i+"): "+
-													rs.getObject(i));
+												BigDecimalHandler.getObjectString(rs,i));
 					}
 					catch (SQLException e) {
 						System.out.println(
@@ -382,13 +383,13 @@ public class resultset {
 						// this does not test the values returned, just whether it gives an exception.
 						if (i>11)
 						{
-							rs.getObject(columnNames[i-1]);
+							BigDecimalHandler.getObjectString(rs,columnNames[i-1],i);
 							System.out.println("getObject("+columnNames[i-1]+") is ok ");
 						}
 						else
 							System.out.println("getObject("+
 										columnNames[i-1]+ "): "+
-										rs.getObject(columnNames[i-1]));
+										BigDecimalHandler.getObjectString(rs,columnNames[i-1],i));
 					}
 					catch (SQLException e) {
 						System.out.println(
