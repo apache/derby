14d31f942e8146fa512ceab2c0297b52e90c1723
Daniel John Debrunner <djd@apache.org>
2006-01-05 17:38:00 +0000

    DERBY-798 Fix NullPointerException when Derby is loaded by the bootstrap class loader in a jvm which returns null on Class.getClassLoader(). Fix suggested by Timothy Luksha (RIT Student).
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@366226 13f79535-47bb-0310-9956-ffa450edef68

--- java/engine/org/apache/derby/impl/services/monitor/BaseMonitor.java
+++ java/engine/org/apache/derby/impl/services/monitor/BaseMonitor.java
@@ -1275,8 +1275,10 @@ nextModule:
 
         ClassLoader cl = getClass().getClassLoader();
         try {
-            for( Enumeration e = cl.getResources("org/apache/derby/modules.properties");
-                 e.hasMoreElements() ;) {
+        	Enumeration e = cl == null ?
+        		ClassLoader.getSystemResources("org/apache/derby/modules.properties") :
+        		cl.getResources("org/apache/derby/modules.properties");
+            while (e.hasMoreElements()) {
                 URL modulesPropertiesURL = (URL) e.nextElement();
                 InputStream is = null;
                 try {
