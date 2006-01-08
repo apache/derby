4df9f63054738d139b60c144eb43d79f14d67bd8
Katherine Marsden <kmarsden@apache.org>
2006-01-08 05:34:06 +0000

    DERBY-398 - Include tests for J2ME/CDC/foundation
    * lang/wisconsin.sql test currenly did not work in J2ME environment because the test setup uses stored procedures with nested connection. To make this test run in this environment, I moved the setup (sql for table creation and inserts) to a Java class (wisconsin). From the Java class, the remaining sql is run using ij.
    * Modified master files
    * Modified the test name from wisconsin.sql to wisconsin.java in the runall and exclude files
    
    Ran lang/wisconsin.java successfully using Sun JDK1.4.2 in all 3 frameworks and also with j9_foundation in IBM WCTME5.7. Please take a look at this patch. Thanks.
    
    Contributed by Deepa Remesh
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@366987 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/testing/org/apache/derbyTesting/functionTests/tests/lang/wisconsin.java
@@ -0,0 +1,184 @@
+/*
+
+Derby - Class org.apache.derbyTesting.functionTests.tests.lang.wisconsin
+
+Copyright 1999, 2004 The Apache Software Foundation or its licensors, as applicable.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+package org.apache.derbyTesting.functionTests.tests.lang;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.derby.iapi.tools.i18n.LocalizedInput;
+import org.apache.derby.iapi.tools.i18n.LocalizedOutput;
+import org.apache.derby.iapi.tools.i18n.LocalizedResource;
+import org.apache.derby.impl.tools.ij.utilMain;
+import org.apache.derby.tools.ij;
+import java.util.Hashtable;
+
+public class wisconsin {
+
+	public static void main(String[] args) throws Throwable{
+		ij.getPropertyArg(args); 
+        Connection conn = ij.startJBMS();
+        
+        conn.setAutoCommit(false);
+        conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+        
+        createTables(conn);
+        
+		utilMain	utilInstance;
+        LocalizedInput is;
+        BufferedInputStream inStream;
+        
+		// set input stream
+		String filePath = "wisconsin.sql";
+
+		try 
+		{
+			inStream = new BufferedInputStream(new FileInputStream(filePath), 
+							utilMain.BUFFEREDFILESIZE);		
+		} catch (FileNotFoundException e)
+		{
+			System.out.println("unable to find input file "+filePath);
+			throw e;
+		}
+        is = LocalizedResource.getInstance().getNewInput(inStream);
+
+		LocalizedInput [] in = { is };
+		LocalizedOutput out = new LocalizedOutput(System.out);
+		
+	
+		utilInstance = new utilMain(1, out, (Hashtable)null);
+		utilInstance.go(in, out, (java.util.Properties) null);
+		//log.flush();
+		out.flush();
+        
+        
+	}
+	
+	private static void createTables(Connection conn) throws SQLException{
+		Statement stmt = conn.createStatement();
+		
+		stmt.execute("create table TENKTUP1 ( unique1 int not null, " +
+											 "unique2 int not null, " +
+											 "two int, " +
+											 "four int, " +
+											 "ten int, " +
+											 "twenty int, " +
+											 "onePercent int, " +
+											 "tenPercent int, " +
+											 "twentyPercent int, " +
+											 "fiftyPercent int, " +
+											 "unique3 int, " +
+											 "evenOnePercent int, " +
+											 "oddOnePercent int, " +
+											 "stringu1 char(52) not null, " +
+											 "stringu2 char(52) not null, " +
+											 "string4 char(52) )");
+		//--insert 10000 rows into TENKTUP1
+		WISCInsert wi = new WISCInsert();
+		wi.doWISCInsert(10000, "TENKTUP1", conn);
+		
+		stmt.execute("create unique index TK1UNIQUE1 on TENKTUP1(unique1)");
+		stmt.execute("create unique index TK1UNIQUE2 on TENKTUP1(unique2)");
+		stmt.execute("create index TK1TWO on TENKTUP1(two)");
+		stmt.execute("create index TK1FOUR on TENKTUP1(four)");
+		stmt.execute("create index TK1TEN on TENKTUP1(ten)");
+		stmt.execute("create index TK1TWENTY on TENKTUP1(twenty)");
+		stmt.execute("create index TK1ONEPERCENT on TENKTUP1(onePercent)");
+		stmt.execute("create index TK1TWENTYPERCENT on TENKTUP1(twentyPercent)");
+		stmt.execute("create index TK1EVENONEPERCENT on TENKTUP1(evenOnePercent)");
+		stmt.execute("create index TK1ODDONEPERCENT on TENKTUP1(oddOnePercent)");
+		stmt.execute("create unique index TK1STRINGU1 on TENKTUP1(stringu1)");
+		stmt.execute("create unique index TK1STRINGU2 on TENKTUP1(stringu2)");
+		stmt.execute("create index TK1STRING4 on TENKTUP1(string4)");
+		
+		stmt.execute("create table TENKTUP2 (unique1 int not null, " +
+											"unique2 int not null, " +
+											"two int, " +
+											"four int, " +
+											"ten int, " +
+											"twenty int, " +
+											"onePercent int, " +
+											"tenPercent int, " +
+											"twentyPercent int, " +
+											"fiftyPercent int, " +
+											"unique3 int, " +
+											"evenOnePercent int, " +
+											"oddOnePercent int, " +
+											"stringu1 char(52), " +
+											"stringu2 char(52), " +
+											"string4 char(52) )");
+		//-- insert 10000 rows into TENKTUP2
+		wi = new WISCInsert();
+		wi.doWISCInsert(10000, "TENKTUP2", conn);
+		
+		stmt.execute("create unique index TK2UNIQUE1 on TENKTUP2(unique1)");
+		stmt.execute("create unique index TK2UNIQUE2 on TENKTUP2(unique2)");
+		
+		stmt.execute("create table ONEKTUP ( unique1 int not null, " +
+											"unique2 int not null, " +
+											"two int, " +
+											"four int, " +
+											"ten int, " +
+											"twenty int, " +
+											"onePercent int, " +
+											"tenPercent int, " +
+											"twentyPercent int, " +
+											"fiftyPercent int, " +
+											"unique3 int, " +
+											"evenOnePercent int, " +
+											"oddOnePercent int, " +
+											"stringu1 char(52), " +
+											"stringu2 char(52), " +
+											"string4 char(52) )");
+		
+		//-- insert 1000 rows into ONEKTUP
+		wi = new WISCInsert();
+		wi.doWISCInsert(1000, "ONEKTUP", conn);
+		
+		stmt.execute("create unique index ONEKUNIQUE1 on ONEKTUP(unique1)");
+		stmt.execute("create unique index ONEKUNIQUE2 on ONEKTUP(unique2)");
+
+		stmt.execute("create table BPRIME (	 unique1 int, " +
+										  	"unique2 int, " +
+											"two int, " +
+											"four int, " +
+											"ten int, " +
+											"twenty int, " +
+											"onePercent int, " +
+											"tenPercent int, " +
+											"twentyPercent int, " +
+											"fiftyPercent int, " +
+											"unique3 int, " +
+											"evenOnePercent int, " +
+											"oddOnePercent int, " +
+											"stringu1 char(52), " +
+											"stringu2 char(52), " +
+											"string4 char(52))");
+
+		stmt.execute("insert into BPRIME select * from TENKTUP2 where TENKTUP2.unique2 < 1000");
+
+		conn.commit();
+
+	}
+	
+}
