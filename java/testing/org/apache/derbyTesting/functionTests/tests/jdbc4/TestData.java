527d377fd95738cb428326863a07d2f5ef09ec8e
Richard N. Hillegas <rhillegas@apache.org>
2006-03-18 16:13:00 +0000

    Commit Anurag's patch for DERBY-942: wiring JDBC4 Ease-of-development calls up to the default QueryObjectGenerator
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@386861 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/testing/org/apache/derbyTesting/functionTests/tests/jdbc4/TestData.java
@@ -0,0 +1,47 @@
+/*
+ 
+   Derby - Class org.apache.derbyTesting.functionTests.tests.jdbc4.TestData
+ 
+   Copyright 2005 The Apache Software Foundation or its licensors, as applicable.
+ 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+ 
+      http://www.apache.org/licenses/LICENSE-2.0
+ 
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ 
+ */
+
+package org.apache.derbyTesting.functionTests.tests.jdbc4;
+
+/**
+ * Data Object clas for TestQueryObject
+ * Fields of this object maps to that of querytable
+ */
+public class TestData {
+    
+    private int id;
+    private String data;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }    
+}
