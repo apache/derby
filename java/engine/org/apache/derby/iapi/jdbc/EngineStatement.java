4e3b1b6bc1bd896dbd242bfaef2359402edbbd7b
Daniel John Debrunner <djd@apache.org>
2006-04-05 17:55:53 +0000

    DERBY-1015 (partial) Add an interface for Statement objects from the engine, EngineStatement. Removes some use of reflection in brokered objects and the network server. Is also a step towards solving DERBY-1158, downgrading holdabilty in global transactions.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@391691 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/engine/org/apache/derby/iapi/jdbc/EngineStatement.java
@@ -0,0 +1,48 @@
+/*
+
+   Derby - Class org.apache.derby.iapi.jdbc.EngineConnection
+
+   Copyright 2006 The Apache Software Foundation or its licensors, as applicable.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+package org.apache.derby.iapi.jdbc;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ * Additional methods the embedded engine exposes on its Statement object
+ * implementations. An internal api only, mainly for the network
+ * server. Allows consistent interaction between emebdded statements
+ * and brokered statements.
+ * 
+ */
+public interface EngineStatement extends Statement {
+    
+    /**
+     * Identical to the JDBC 3 getMoreResults(int).
+     * 
+     * @see java.sql.Statement#getMoreResults(int)
+     */
+    public boolean getMoreResults(int current) throws SQLException;
+    
+    /**
+     * Identical to the JDBC 3 getResultSetHoldability(int).
+     * 
+     * @see java.sql.Statement#getResultSetHoldability()
+     */ 
+    public int getResultSetHoldability() throws SQLException;
+}
