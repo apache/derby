6737dea016d2c36e2449aeb1bae25263aad34e46
Knut Anders Hatlen <kahatlen@apache.org>
2006-06-02 18:00:17 +0000

    DERBY-1262: Like-predicates: % does not match tab character
    Merged fix from trunk.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/branches/10.1@411223 13f79535-47bb-0310-9956-ffa450edef68

--- java/engine/org/apache/derby/impl/sql/compile/LikeEscapeOperatorNode.java
+++ java/engine/org/apache/derby/impl/sql/compile/LikeEscapeOperatorNode.java
@@ -554,7 +554,9 @@ public final class LikeEscapeOperatorNode extends TernaryOperatorNode
 				return this;
 			}
 
-			greaterEqualString = Like.greaterEqualString(pattern, escape);
+			int maxWidth = receiver.getTypeServices().getMaximumWidth();
+			greaterEqualString = Like.greaterEqualString(pattern, escape, 
+														 maxWidth);
 
 
 
@@ -563,7 +565,7 @@ public final class LikeEscapeOperatorNode extends TernaryOperatorNode
 			 */
             if ( ! receiver.getTypeId().isNationalStringTypeId() )
 			{
-				lessThanString = Like.lessThanString(greaterEqualString);
+				lessThanString = Like.lessThanString(pattern, escape, maxWidth);
 				eliminateLikeComparison = ! Like.isLikeComparisonNeeded(pattern);
 			}
 		}
@@ -610,8 +612,9 @@ public final class LikeEscapeOperatorNode extends TernaryOperatorNode
 			QueryTreeNode likeLTopt;
 			if (leftOperand.isParameterNode())
 			{
+				int maxWidth = receiver.getTypeServices().getMaximumWidth();
 				likeLTopt = setupOptimizeStringFromParameter(leftOperand, rightOperand,
-								"lessThanStringFromParameter");
+								"lessThanStringFromParameter", maxWidth);
 			}
 			else
 			{
@@ -655,8 +658,9 @@ public final class LikeEscapeOperatorNode extends TernaryOperatorNode
 			// Create an expression off the parameter
 			// new SQLChar(Like.greaterEqualString(?));
 
+			int maxWidth = receiver.getTypeServices().getMaximumWidth();
 			likeGEopt = setupOptimizeStringFromParameter(leftOperand, rightOperand,
-									"greaterEqualStringFromParameter");
+								"greaterEqualStringFromParameter", maxWidth);
 
 		} else {
 
@@ -789,7 +793,7 @@ public final class LikeEscapeOperatorNode extends TernaryOperatorNode
 	}
 
 	private ValueNode setupOptimizeStringFromParameter(ValueNode parameterNode,
-						ValueNode escapeNode,String methodName)
+						ValueNode escapeNode,String methodName, int maxWidth)
 		throws StandardException {
 
 		Vector param;
@@ -813,6 +817,12 @@ public final class LikeEscapeOperatorNode extends TernaryOperatorNode
 		param.addElement(parameterNode);
 		if (escapeNode != null)
 			param.addElement(escapeNode);
+
+		QueryTreeNode maxWidthNode = getNodeFactory().getNode(
+										C_NodeTypes.INT_CONSTANT_NODE,
+										new Integer(maxWidth),
+										getContextManager());
+		param.addElement(maxWidthNode);
 
 		methodCall.addParms(param);
 
