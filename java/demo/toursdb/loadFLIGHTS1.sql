44e245574f9676b09ad21d7e6cfab1be6bfc09c8
Samuel Andrew McIntyre <fuzzylogic@apache.org>
2006-08-08 21:30:24 +0000

    DERBY-1377: Update license headers in java/demo
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@429826 13f79535-47bb-0310-9956-ffa450edef68

--- java/demo/toursdb/loadFLIGHTS1.sql
+++ java/demo/toursdb/loadFLIGHTS1.sql
@@ -1,3 +1,18 @@
+-- Licensed to the Apache Software Foundation (ASF) under one or more
+-- contributor license agreements.  See the NOTICE file distributed with
+-- this work for additional information regarding copyright ownership.
+-- The ASF licenses this file to You under the Apache License, Version 2.0
+-- (the "License"); you may not use this file except in compliance with
+-- the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 insert into FLIGHTS values ('AA1111',1,'ABQ','09:00:00','LAX','09:19:00','S',1.328,664,'B747'); 
 insert into FLIGHTS values ('AA1112',1,'LAX','09:00:00','ABQ','11:19:00','S',1.328,664,'B747'); 
 insert into FLIGHTS values ('AA1113',1,'ABQ','09:00:00','PHX','09:39:00','S',0.658,329,'B747'); 
--- java/demo/toursdb/loadFLIGHTS2.sql
+++ java/demo/toursdb/loadFLIGHTS2.sql
@@ -1,3 +1,18 @@
+-- Licensed to the Apache Software Foundation (ASF) under one or more
+-- contributor license agreements.  See the NOTICE file distributed with
+-- this work for additional information regarding copyright ownership.
+-- The ASF licenses this file to You under the Apache License, Version 2.0
+-- (the "License"); you may not use this file except in compliance with
+-- the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 insert into FLIGHTS values ('AA1137',2,'KBL','13:30:00','SVO','15:41:00','S',4.192,2096,'A320'); 
 insert into FLIGHTS values ('AA1138',1,'SVO','09:00:00','KBL','15:11:00','L',4.192,2096,'A320'); 
 insert into FLIGHTS values ('US1321',1,'KHI','09:00:00','IST','10:54:00','L',4.9,2450,'B747'); 
