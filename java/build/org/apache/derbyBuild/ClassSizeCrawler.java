4ecc65a1abde7b94c63da0b8fac4282ed5b8ebdf
Samuel Andrew McIntyre <fuzzylogic@apache.org>
2006-08-09 21:47:16 +0000

    DERBY-1377 (partial): cleanup a couple of missed files, insert proper license header into the generated ClassSizeCrawler.java
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@430149 13f79535-47bb-0310-9956-ffa450edef68

--- java/build/org/apache/derbyBuild/ClassSizeCrawler.java
+++ java/build/org/apache/derbyBuild/ClassSizeCrawler.java
@@ -186,8 +186,21 @@ public class ClassSizeCrawler
             int year = cal.get( Calendar.YEAR);
             PrintWriter out = new PrintWriter( new FileWriter( outputFileName));
             out.print( "/*\n\n" +
-                       "    Copyright " + year + " The Apache Software Foundation or its licensors, as applicable.\n" +
-                       "    Licensed under the Apache License, Version 2.0 (the \"License\").\n" +
+
+                       "   Licensed to the Apache Software Foundation (ASF) under one or more\n" +
+                       "   contributor license agreements.  See the NOTICE file distributed with\n" +
+                       "   this work for additional information regarding copyright ownership.\n" +
+                       "   The ASF licenses this file to You under the Apache License, Version 2.0\n" +
+                       "   (the \"License\"); you may not use this file except in compliance with\n" +
+                       "   the License.  You may obtain a copy of the License at\n" +
+                       "\n" +
+                       "      http://www.apache.org/licenses/LICENSE-2.0\n" +
+                       "\n" +
+                       "   Unless required by applicable law or agreed to in writing, software\n" +
+                       "   distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                       "   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                       "   See the License for the specific language governing permissions and\n" +
+                       "   limitations under the License.\n" +
                        " */\n");
             out.print( "package org.apache.derby.iapi.services.cache;\n" +
                        "import java.util.Hashtable;\n" +
