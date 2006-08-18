12e892f542f99d67b65d43138d5527e1c59aeb65
Daniel John Debrunner <djd@apache.org>
2006-08-18 03:43:52 +0000

    Add a couple _Suite JUnit suites in lang and jdbc4, as framework, neither is used yet.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@432479 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/testing/org/apache/derbyTesting/functionTests/tests/jdbc4/_Suite.java
@@ -0,0 +1,70 @@
+/*
+
+   Derby - Class org.apache.derbyTesting.functionTests.tests.jdbcapi._Suite
+
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License
+*/
+package org.apache.derbyTesting.functionTests.tests.jdbc4;
+
+import java.sql.SQLException;
+
+import org.apache.derbyTesting.functionTests.util.BaseTestCase;
+import junit.framework.Test; 
+import junit.framework.TestSuite;
+
+/**
+ * Suite to run all JUnit tests in this package:
+ * org.apache.derbyTesting.functionTests.tests.jdbc4
+ *
+ */
+public class _Suite extends BaseTestCase  {
+
+	/**
+	 * Use suite method instead.
+	 */
+	private _Suite(String name) {
+		super(name);
+	}
+
+	public static Test suite() throws SQLException {
+
+		TestSuite suite = new TestSuite();
+
+		//suite.addTestSuite(AutoloadBooting.class);
+		//suite.addTestSuite(AutoloadTest.class);
+		suite.addTest(BlobTest.suite());
+		suite.addTest(CallableStatementTest.suite());
+		suite.addTest(ClobTest.suite());
+		suite.addTest(ClosedObjectTest.suite());
+		suite.addTest(ConnectionTest.suite());
+		suite.addTest(DataSourceTest.suite());
+		suite.addTestSuite(JDBC40TranslationTest.class);	
+		suite.addTest(ParameterMetaDataWrapperTest.suite());
+		suite.addTest(PreparedStatementTest.suite());
+		suite.addTest(ResultSetMetaDataTest.suite());
+		suite.addTest(ResultSetTest.suite());
+		suite.addTest(RowIdNotImplementedTest.suite());
+		suite.addTest(StatementEventsTest.suite());
+		suite.addTest(StatementTest.suite());
+		suite.addTestSuite(UnsupportedVetter.class);
+		suite.addTest(VerifySignatures.suite());
+		suite.addTest(XA40Test.suite());
+		
+		return suite;
+	}
+}
---
+++ java/testing/org/apache/derbyTesting/functionTests/tests/lang/_Suite.java
@@ -0,0 +1,60 @@
+/*
+
+   Derby - Class org.apache.derbyTesting.functionTests.tests.jdbcapi._Suite
+
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License
+*/
+package org.apache.derbyTesting.functionTests.tests.lang;
+
+import org.apache.derbyTesting.functionTests.util.BaseTestCase;
+import org.apache.derbyTesting.functionTests.util.JDBC;
+
+import junit.framework.Test; 
+import junit.framework.TestSuite;
+
+/**
+ * Suite to run all JUnit tests in this package:
+ * org.apache.derbyTesting.functionTests.tests.lang
+ *
+ */
+public class _Suite extends BaseTestCase  {
+
+	/**
+	 * Use suite method instead.
+	 */
+	private _Suite(String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+
+		TestSuite suite = new TestSuite("lang");
+
+		// suite.addTest(PrepareExecuteDDL.suite());
+		// suite.addTest(LangScripts.suite());
+		
+		// Tests that are compiled using 1.4 target need to
+		// be added this way, otherwise creating the suite
+		// will throw an invalid class version error
+		if (JDBC.vmSupportsJDBC3() || JDBC.vmSupportsJSR169())
+		{
+		}
+
+		return suite;
+	}
+}
