dca0c55dddee95d39892d3c8602ea6e0020b81b8
Richard N. Hillegas <rhillegas@apache.org>
2006-10-25 22:25:46 +0000

    DERBY-1377: Add copyright headers to sql test scripts. Adjust canons accordingly.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@467793 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/i18n/I18NImportExport.sql
+++ java/testing/org/apache/derbyTesting/functionTests/tests/i18n/I18NImportExport.sql
@@ -1,3 +1,19 @@
+--
+--   Licensed to the Apache Software Foundation (ASF) under one or more
+--   contributor license agreements.  See the NOTICE file distributed with
+--   this work for additional information regarding copyright ownership.
+--   The ASF licenses this file to You under the Apache License, Version 2.0
+--   (the "License"); you may not use this file except in compliance with
+--   the License.  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--   Unless required by applicable law or agreed to in writing, software
+--   distributed under the License is distributed on an "AS IS" BASIS,
+--   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--   See the License for the specific language governing permissions and
+--   limitations under the License.
+--
 drop table tab1;
 create table tab1( c1 decimal(5,3), c2 date, c3 char(20) );
 insert into tab1 values(12.345, date('2000-05-25'),  'test row 1');
--- java/testing/org/apache/derbyTesting/functionTests/tests/lang/big.sql
+++ java/testing/org/apache/derbyTesting/functionTests/tests/lang/big.sql
@@ -1,3 +1,19 @@
+--
+--   Licensed to the Apache Software Foundation (ASF) under one or more
+--   contributor license agreements.  See the NOTICE file distributed with
+--   this work for additional information regarding copyright ownership.
+--   The ASF licenses this file to You under the Apache License, Version 2.0
+--   (the "License"); you may not use this file except in compliance with
+--   the License.  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--   Unless required by applicable law or agreed to in writing, software
+--   distributed under the License is distributed on an "AS IS" BASIS,
+--   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--   See the License for the specific language governing permissions and
+--   limitations under the License.
+--
 -- create table with row greater than 32K
 -- try with just one row of data - JCC may handle those differently?
 create table big(c1 varchar(10000), c2 varchar(10000), c3 varchar(10000), c4 varchar(10000));
