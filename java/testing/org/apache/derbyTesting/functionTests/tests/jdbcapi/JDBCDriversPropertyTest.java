814508981dd56172333c37129df15d5943826547
Daniel John Debrunner <djd@apache.org>
2007-01-12 19:37:22 +0000

    DERBY-1952 (partial) Add some junit test cases that are wrappers around jdbcapi.autoloadTest that set jdbc.drivers as a System property before running jdbcapi.autoloadTest fixtures. These tests are intended only to be run standalone, ie a single run on one test class in a jvm, as they test autoloading of Derby's drivers by the DriverManager class.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@495704 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/testing/org/apache/derbyTesting/functionTests/tests/jdbcapi/JDBCDriversPropertyTest.java
@@ -0,0 +1,70 @@
+/*
+
+   Derby - Class org.apache.derbyTesting.functionTests.tests.jdbcapi.AutoloadTest
+
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to you under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+package org.apache.derbyTesting.functionTests.tests.jdbcapi;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Run an autoload test with the system property
+ * jdbc.drivers being set. This test is only intended
+ * to be run in its own vm as a single test run.
+ * It inherits from TestCase so as to not have any
+ * chance of loading DriverManager before setting
+ * jdbc.drivers. 
+ */
+abstract class JDBCDriversPropertyTest extends TestCase {
+    
+    final static Test getSuite(String jdbcDrivers) throws Exception {
+        
+        TestSuite suite = new TestSuite("jdbc.drivers="+jdbcDrivers);
+        
+        System.setProperty("jdbc.drivers", jdbcDrivers);
+        
+        suite.addTest(getAutoLoadSuite());
+            
+        return suite;
+    }
+    
+    /**
+     * Load the class and run its suite method through reflection
+     * so that DriverManger is not loaded indirectly before
+     * jdbc.drivers is set.
+     */
+    private static Test getAutoLoadSuite()
+       throws Exception
+    {
+        Class alt = Class.forName(
+           "org.apache.derbyTesting.functionTests.tests.jdbcapi.AutoloadTest");
+        
+        Method suiteMethod = alt.getMethod("suite", null);
+        
+        return (Test) suiteMethod.invoke(null, null);
+    }
+    
+    JDBCDriversPropertyTest() {
+        super();
+     }
+}
