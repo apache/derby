a8ccb07f5bf6c3ae9487c02fee111d60c4b4f858
Knut Anders Hatlen <kahatlen@apache.org>
2007-03-29 12:58:03 +0000

    DERBY-2493 (partial) Use unsynchronized collections in BackingStoreHashtable
    Updated some tests so that they are not sensitive to the order of the
    rows in the returned ResultSet.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@523691 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/lang/GroupByTest.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/lang/GroupByTest.java
@@ -48,5 +48,67 @@ public class GroupByTest extends BaseJDBCTestCase {
 		s.execute("declare global temporary table session.ztemp ( orderID varchar( 50 ) ) not logged");
 		JDBC.assertEmpty(s.executeQuery("select orderID from session.ztemp group by orderID"));
 	}
+
+	/**
+	 * DERBY-280: Wrong result from select when aliasing to same name as used
+	 * in group by
+	 */
+	public void testGroupByWithAliasToSameName() throws SQLException {
+		// disable auto-commit so that no tear-down code is needed
+		getConnection().setAutoCommit(false);
+
+		Statement s = createStatement();
+		s.executeUpdate("create table bug280 (a int, b int)");
+		s.executeUpdate("insert into bug280 (a, b) " +
+						"values (1,1), (1,2), (1,3), (2,1), (2,2)");
+
+		String[][] expected1 = {{"1", "3"}, {"2", "2"}};
+		JDBC.assertUnorderedResultSet(
+			s.executeQuery("select a, count(a) from bug280 group by a"),
+			expected1);
+		// The second query should return the same results as the first. Would
+		// throw exception before DERBY-681.
+		JDBC.assertUnorderedResultSet(
+			s.executeQuery("select a, count(a) as a from bug280 group by a"),
+			expected1);
+
+		// should return same results as first query (but with extra column)
+		String[][] expected2 = {{"1", "3", "1"}, {"2", "2", "2"}};
+		JDBC.assertUnorderedResultSet(
+			s.executeQuery("select a, count(a), a from bug280 group by a"),
+			expected2);
+
+		// different tables with same column name ok
+		String[][] expected3 = {
+			{"1","1"}, {"1","1"}, {"1","1"}, {"2","2"}, {"2","2"} };
+		JDBC.assertFullResultSet(
+			s.executeQuery("select t.t_i, m.t_i from " +
+						   "(select a, b from bug280 group by a, b) " +
+						   "t (t_i, t_dt), " +
+						   "(select a, b from bug280 group by a, b) " +
+						   "m (t_i, t_dt) " +
+						   "where t.t_i = m.t_i and t.t_dt = m.t_dt " +
+						   "group by t.t_i, t.t_dt, m.t_i, m.t_dt " +
+						   "order by t.t_i,m.t_i"),
+			expected3);
+
+		// should be allowed
+		String[][] expected4 = { {"1", "1"}, {"2", "2"} };
+		JDBC.assertUnorderedResultSet(
+			s.executeQuery("select a, a from bug280 group by a"),
+			expected4);
+		JDBC.assertUnorderedResultSet(
+			s.executeQuery("select bug280.a, a from bug280 group by a"),
+			expected4);
+		JDBC.assertUnorderedResultSet(
+			s.executeQuery("select bug280.a, bug280.a from bug280 group by a"),
+			expected4);
+		JDBC.assertUnorderedResultSet(
+			s.executeQuery("select a, bug280.a from bug280 group by a"),
+			expected4);
+
+		s.close();
+		rollback();
+	}
 }
 
