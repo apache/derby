483783f33fc06b55c2108654bea1116f331bd7a1
Richard N. Hillegas <rhillegas@apache.org>
2007-04-13 14:25:20 +0000

    DERBY-2544: Disable SecurityPolicyReloadingTest because it fails in the tinderbox environment on java 6 when run in the suite.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@528494 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/lang/_Suite.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/lang/_Suite.java
@@ -83,7 +83,7 @@ public class _Suite extends BaseTestCase  {
         suite.addTest(ScrollCursors2Test.suite());
         suite.addTest(NullIfTest.suite());
         suite.addTest(InListMultiProbeTest.suite());
-        suite.addTest(SecurityPolicyReloadingTest.suite());
+        //suite.addTest(SecurityPolicyReloadingTest.suite());
         suite.addTest(CurrentOfTest.suite());
         suite.addTest(UnaryArithmeticParameterTest.suite());
         suite.addTest(HoldCursorTest.suite());
