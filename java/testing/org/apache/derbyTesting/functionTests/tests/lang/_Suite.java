6c9ce644b02df27f6f1d3d6eae8ade44df0af606
Katherine Marsden <kmarsden@apache.org>
2007-06-26 21:04:58 +0000

    DERBY-2727 backout change to convert AggregateClassLoading.java until IBM 1.5 failures are resolved.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@550950 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/lang/_Suite.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/lang/_Suite.java
@@ -110,7 +110,6 @@ public class _Suite extends BaseTestCase  {
         suite.addTest(SpillHashTest.suite());
         suite.addTest(CaseExpressionTest.suite());
         suite.addTest(CharUTF8Test.suite());
-        suite.addTest(AggregateClassLoadingTest.suite());
 
         // Add the XML tests, which exist as a separate suite
         // so that users can "run all XML tests" easily.
