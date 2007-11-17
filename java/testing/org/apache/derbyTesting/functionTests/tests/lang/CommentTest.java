449a1ebad5145bbc9096499a779d1cb8b3bdda48
Dag H. Wanvik <dag@apache.org>
2007-11-17 02:00:07 +0000

    DERBY-1749 Implement Bracketed SQL comments (/*...*/ comments)
    Fixed wrong class name in the comment header.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@595892 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/lang/CommentTest.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/lang/CommentTest.java
@@ -1,6 +1,6 @@
 /*
 
-   Derby - Class org.apache.derbyTesting.functionTests.tests.lang.CreateTableFromQueryTest
+   Derby - Class org.apache.derbyTesting.functionTests.tests.lang.CommentTest
 
        Licensed to the Apache Software Foundation (ASF) under one
        or more contributor license agreements.  See the NOTICE file
