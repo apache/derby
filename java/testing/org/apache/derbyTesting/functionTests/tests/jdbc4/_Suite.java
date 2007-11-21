b12505f2e5818f645b56ec7ec7a0201ca793f52a
Myrna van Lunteren <myrnavl@apache.org>
2007-11-21 21:35:27 +0000

    correct name in header
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@597215 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/jdbc4/_Suite.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/jdbc4/_Suite.java
@@ -1,6 +1,6 @@
 /*
 
-   Derby - Class org.apache.derbyTesting.functionTests.tests.jdbcapi._Suite
+   Derby - Class org.apache.derbyTesting.functionTests.tests.jdbc4._Suite
 
        Licensed to the Apache Software Foundation (ASF) under one
        or more contributor license agreements.  See the NOTICE file
