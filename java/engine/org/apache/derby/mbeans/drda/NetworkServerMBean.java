fbefd2572d36d98a0872aa85506aebf6cfdee9e8
Daniel John Debrunner <djd@apache.org>
2008-02-20 17:08:07 +0000

    DERBY-3435 Add NetworkServerMBean interface for JMX monitoring of network server. Contributed by John H. Embretsen Email: John dot Embretsen at Sun dot com
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@629536 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/engine/org/apache/derby/mbeans/drda/NetworkServerMBean.java
@@ -0,0 +1,156 @@
+/*
+    
+   Derby - Class org.apache.derby.mbeans.drda.NetworkServerMBean
+
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+
+package org.apache.derby.mbeans.drda;
+
+/**
+ * This is an MBean defining a JMX management and monitoring interface of 
+ * Derby's Network Server.
+ * This interface consists of getter and setter methods for attributes that may
+ * be read and/or modified, and methods representing operations that can be 
+ * invoked.
+ * 
+ * For more information on Managed Beans, refer to the JMX specification.
+ *
+ * @see org.apache.derby.drda.NetworkServerControl
+ *
+ */
+public interface NetworkServerMBean {
+    
+    // ---
+    // ----------------- MBean attributes ------------------------------------
+    // ---
+    
+    // Commented setters because:
+    //   No attribute setting yet due to security concerns, see DERBY-1387.
+    
+    /**
+     * Gets the value of the <code>derby.drda.host</code> network server
+     * setting. In this context, the host defines the network interface on which
+     * the Network Server is listening for connections. "<code>0.0.0.0</code>" 
+     * means that the server allows connections from any host on the network.
+     * 
+     * @return the value of <code>derby.drda.host</code>
+     */
+    public String getDrdaHost();
+    
+    /**
+     * Gets the value of the <code>derby.drda.keepAlive</code> network server
+     * setting. 
+     * 
+     * @see <a href="http://db.apache.org/derby/docs/dev/adminguide/radmindrdakeepalive.html"><code>derby.drda.keepAlive</code> documentation</a>
+     * @return the value of <code>derby.drda.keepAlive</code>
+     */
+    public boolean getDrdaKeepAlive();
+    
+    /**
+     * Gets the value of the <code>derby.drda.maxThreads</code> network server 
+     * setting.
+     * @return the value of the <code>derby.drda.maxThreads</code> network 
+     *         server setting
+     */
+    public int getDrdaMaxThreads();
+    //public void setDrdaMaxThreads(int max) throws Exception;
+    
+    /**
+     * Gets the value of the <code>derby.drda.portNumber</code> network server
+     * setting. This is the port number on which the Network Server is listening
+     * for client connections.
+     * 
+     * @return the port number on which the Network Server is listening
+     *         for client connections.
+     */
+    public int getDrdaPortNumber();
+    
+    /**
+     * Gets the value of the <code>derby.drda.securityMechanism</code> network 
+     * server setting. 
+     * 
+     * @return the value of the <code>derby.drda.securityMechanism</code> 
+     *         network server setting.
+     */
+    public String getDrdaSecurityMechanism();
+    
+    /**
+     * Gets the value of the <code>derby.drda.sslMode</code> network server 
+     * setting. 
+     * 
+     * @return the value of the <code>derby.drda.sslMode</code> network server 
+     *         setting.
+     */
+    public String getDrdaSslMode();
+    
+    /**
+     * Gets the value of the <code>derby.drda.streamOutBufferSize</code> network
+     * server setting.
+     * This setting is used to configure the size of the buffer used for 
+     * streaming blob/clob from server to client.
+     * 
+     * @return the size of the buffer used for streaming blob/clob from server 
+     *         to client
+     */
+    public String getDrdaStreamOutBufferSize();
+    
+    /**
+     * Gets the value of the <code>derby.drda.timeSlice</code> network server 
+     * setting.
+     * @return the value of the <code>derby.drda.timeSlice</code> network 
+     *         server setting
+     */
+    public int getDrdaTimeSlice();
+    //public void setDrdaTimeSlice(int timeSlice) throws Exception;
+    
+    /**
+     * Gets the value of the <code>derby.drda.traceAll</code> network server 
+     * setting.
+     * @return the value of the <code>derby.drda.traceAll</code> network 
+     *         server setting
+     */
+    public boolean getDrdaTraceAll();
+    //public void setDrdaTraceAll(boolean on) throws Exception;
+    
+    /**
+     * Gets the value of the <code>derby.drda.traceDirectory</code> network 
+     * server setting. If this setting has not been explicitly set by the
+     * network server administrator, the default value is returned.
+     * @return the value of the <code>derby.drda.timeSlice</code> network 
+     *         server setting
+     */
+    public String getDrdaTraceDirectory();
+    //public void setDrdaTraceDirectory(String dir) throws Exception;
+
+    
+    // ---
+    // ----------------- MBean operations ------------------------------------
+    // ---
+
+    /**
+     * Executes the network server's <code>ping</code> command.
+     * Returns without errors if the server was successfully pinged.
+     * @throws java.lang.Exception if the ping attempt fails (an indication that
+     *         the network server is not running properly)
+     */
+    public void ping() throws Exception;
+    
+    // No other management operations yet due to security concerns, see 
+    // DERBY-1387 for details.
+    
+}
