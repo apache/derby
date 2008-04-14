dfce7ede8a697e13e539263bc81d129ddd55ca0a
Kristian Waagan <kristwaa@apache.org>
2008-04-14 07:39:29 +0000

    DERBY-2892:  Closing a resultset after retrieving a large > 32665 bytes value with Network Server does not release locks. Cleanup merged from trunk revision 647680 (derby-2892-2a-alternative_fix_cleanup.diff).
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/branches/10.4@647682 13f79535-47bb-0310-9956-ffa450edef68

--- java/client/org/apache/derby/client/am/ClobLocatorInputStream.java
+++ java/client/org/apache/derby/client/am/ClobLocatorInputStream.java
@@ -64,13 +64,6 @@ public class ClobLocatorInputStream extends java.io.InputStream {
     private long currentPos;
     
     /**
-     * If true, the underlying Blob will be freed when the underlying stream is
-     * closed.  Used to implement correct behavior for streams obtained from
-     * result sets.
-     */
-    private boolean freeClobOnClose = false;
-
-    /**
      * Create an <code>InputStream</code> for reading the
      * <code>Clob</code> value represented by the given locator based
      * <code>Clob</code> object.
@@ -166,39 +159,6 @@ public class ClobLocatorInputStream extends java.io.InputStream {
             throw ioEx;
         }
     }
-
-    /**
-     * Closes this input stream and releases any system resources associated
-     * with the stream.  This will release the underlying Clob value. 
-     *  
-     * @throws java.io.IOException
-     */
-    public void close() throws IOException {
-        try {
-            if (clob != null  && freeClobOnClose) {
-                clob.free();
-            }
-        } catch (SQLException ex) {
-            if (ex.getSQLState().compareTo
-                    (ExceptionUtil.getSQLStateFromIdentifier
-                            (SQLState.LOB_OBJECT_INVALID)) == 0) {
-                // Clob has already been freed, probably because of autocommit
-                return;  // Ignore error
-            }
-
-            IOException ioEx = new IOException();
-            ioEx.initCause(ex);
-            throw ioEx;
-        }
-    }
-      
-    /**
-     * Tell stream to free the underlying Clob when it is closed.
-     */
-    public void setFreeClobOnClose() {
-        freeClobOnClose = true;
-    }
-    
 
     /**
      * Returns a <code>Byte</code> array from the
