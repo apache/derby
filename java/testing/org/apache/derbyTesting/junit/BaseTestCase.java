9a498155ce3f6c115bc2862709d54b364dd077e7
Katherine Marsden <kmarsden@apache.org>
2008-05-27 12:51:15 +0000

    DERBY-3546 Failed to get database schemas of a JAR database
    fix javadoc warning from previous checkin.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/branches/10.3@660495 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/junit/BaseTestCase.java
+++ java/testing/org/apache/derbyTesting/junit/BaseTestCase.java
@@ -388,7 +388,7 @@ public abstract class BaseTestCase
     }
     /**
      * Remove the directory and its contents.
-     * @param fir File of the directory
+     * @param dir File of the directory
      */
     public static void removeDirectory(File dir)
     {
