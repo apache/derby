2a899d807065672478e3563c09b2119f2900f675
Katherine Marsden <kmarsden@apache.org>
2008-07-15 12:11:25 +0000

    DERBY-3777 SecureServerTest, SSLTest  failed on Zos with exit code 143 starting network server
    Extend wait time for network server to come up.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@676894 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/junit/NetworkServerTestSetup.java
+++ java/testing/org/apache/derbyTesting/junit/NetworkServerTestSetup.java
@@ -41,16 +41,13 @@ import org.apache.derby.drda.NetworkServerControl;
  */
 final public class NetworkServerTestSetup extends BaseTestSetup {
 
-    /** Setting maximum wait time to 300 seconds.   For some systems it looks
-     *  like restarting a server to listen on the same port is blocked waiting
-     *  for a system specific interval.  This number looks to be something
-     *  like 240 seconds on XP.  Waiting shorter than this time causes
-     *  intermittent failures on a laptop running XP with a software firewall
-     *  and a VPN.  Increasing the wait time should not adversely affect those
+    /** Setting maximum wait time to 40 seconds.   On some platforms
+     * it may take this long to start the server.  Increasing the wait
+     *  time should not adversely affect those
      *  systems with fast port turnaround as the actual code loops for 
      *  SLEEP_TIME intervals, so should never see WAIT_TIME.
      */
-    private static final long WAIT_TIME = 10000;
+    private static final long WAIT_TIME = 40000;
     
     /** Sleep for 500 ms before pinging the network server (again) */
     private static final int SLEEP_TIME = 100;
