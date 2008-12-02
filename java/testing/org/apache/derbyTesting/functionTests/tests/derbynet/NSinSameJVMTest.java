c38c5e1d1f67fd579bf1545cde0f2b16608edf72
Myrna van Lunteren <myrnavl@apache.org>
2008-12-02 22:25:01 +0000

    DERBY-1465; backing out changes of revision 718616; they cause an intermittent   hang.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@722639 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/derbynet/NSinSameJVMTest.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/derbynet/NSinSameJVMTest.java
@@ -54,17 +54,6 @@ public class NSinSameJVMTest extends BaseJDBCTestCase {
         ResultSet rs = stmt
         .executeQuery("Select  tablename   from  sys.systables");
         JDBC.assertDrainResults(rs);
-        
-        // DERBY-1465 - starting another server on the same
-        // port fails and should throw an exception as well as log it
-        try { 
-            serverControl.start(null);
-            // commenting out until intermittent failure has been resolved.
-	    //fail ("Should have gotten an exception - see DERBY-1465");
-        } catch (Exception e) {
-            assertTrue(e.getMessage().indexOf("java.net.BindException") > 1);
-        }
-        
         // Leave the connection open before shutdown
         serverControl.shutdown();
     }
