9c2b1f3f666e85d340c5d5fa0b8cb84f59a7b17c
Dag H. Wanvik <dag@apache.org>
2009-01-05 17:25:31 +0000

    Fixed some erroneous header comments: wrong class names from cut/paste presumably
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@731628 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/replicationTests/ReplicationRun_Local.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/replicationTests/ReplicationRun_Local.java
@@ -1,6 +1,6 @@
 /*
  
-Derby - Class org.apache.derbyTesting.functionTests.tests.replicationTests.ReplicationRun
+Derby - Class org.apache.derbyTesting.functionTests.tests.replicationTests.ReplicationRun_Local
  
 Licensed to the Apache Software Foundation (ASF) under one or more
 contributor license agreements.  See the NOTICE file distributed with
