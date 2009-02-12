1b71e2713c5e6e871e04ceae57bf2e88b19edd12
Katherine Marsden <kmarsden@apache.org>
2009-02-12 16:43:11 +0000

    DERBY-4050 Multithreaded clob update causes growth in table that does not get reclaimed merge 743023 from trunk.  Add static SystemPropertyTestSetup which wasn't in 10.4
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/branches/10.4@743799 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/store/_Suite.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/store/_Suite.java
@@ -57,6 +57,7 @@ public class _Suite extends BaseTestCase  {
         suite.addTest(PositionedStoreStreamTest.suite());
         suite.addTest(BackupRestoreTest.suite());
         suite.addTest(OfflineBackupTest.suite());
+        suite.addTest(ClobReclamationTest.suite());
         // Encryption only supported for Derby in J2SE/J2EE environments.
         // J2ME (JSR169) does not support encryption.
         if (JDBC.vmSupportsJDBC3()) {
