b472c868aab0db57fe60c030795cd4225da714b4
Dag H. Wanvik <dag@apache.org>
2009-12-04 18:10:06 +0000

    DERBY-4461
    Committed fix which resets the interrupt flag in the test's
    application thread.  Not resetting it caused issues on Windows and
    Linux downstream (in teardown).
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@887305 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/store/Derby151Test.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/store/Derby151Test.java
@@ -150,6 +150,12 @@ public class Derby151Test extends BaseJDBCTestCase
             println("Not able to test fix for DERBY-151: No interrupt seen");
         } catch (SQLException e) {
             assertSQLState("XSDG9", e);
+            // Clearing the interrupt flag; makes sense for embedded part of
+            // the test. When running c/s, it would be the server thread which
+            // receives the interrupt, and since db is shut down, that should
+            // be ok for later tests. We need to clear the flag here since, for
+            // embedded, the app thread is affected.
+            Thread.currentThread().interrupted();
         }
     }
 }
