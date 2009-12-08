136f24b53b031470618a2caeb9471d1b49ad7a91
Knut Anders Hatlen <kahatlen@apache.org>
2009-12-08 10:24:20 +0000

    DERBY-4442: Default value and identity in an INSERT result set evaluated too early
    Back out the fix for DERBY-4419 and DERBY-4425, since the fix is no
    longer necessary after the changes that added a ProjectRestrictNode on
    top of the original insert source. The test cases for those bugs are
    still part of the regression test suite.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@888311 13f79535-47bb-0310-9956-ffa450edef68

--- java/engine/org/apache/derby/impl/sql/compile/ResultColumnList.java
+++ java/engine/org/apache/derby/impl/sql/compile/ResultColumnList.java
@@ -1688,7 +1688,7 @@ public class ResultColumnList extends QueryTreeNodeVector
 			ResultColumn resultColumn = (ResultColumn) elementAt(index);
 
 			/* Skip over generated columns */
-			if (resultColumn.isGenerated() || resultColumn.isGeneratedForUnmatchedColumnInInsert())
+			if (resultColumn.isGenerated())
 			{
 				continue;
 			}
