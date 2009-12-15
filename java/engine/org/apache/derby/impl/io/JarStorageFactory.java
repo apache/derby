49639805553a9ba54604cd962b310455649b10bb
Kristian Waagan <kristwaa@apache.org>
2009-12-15 08:47:36 +0000

    DERBY-4472: [patch] non short circuit op, could cause eof problems Replaced non-short-circuiting bitwise operator with short-circuiting boolean operator (the right side might throw exception if evaluated when the left side is false).
    Patch contributed by Dave Brosius (dbrosius at mebigfatguy dot com).
    
    Patch file: sc.diff
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@890692 13f79535-47bb-0310-9956-ffa450edef68

--- java/engine/org/apache/derby/impl/io/JarStorageFactory.java
+++ java/engine/org/apache/derby/impl/io/JarStorageFactory.java
@@ -84,7 +84,7 @@ public class JarStorageFactory extends BaseStorageFactory
             return;
         // Parse the dataDirectory name. It should be of the form "(jar-file)directory" or "jar-file"
         int offset = 0;
-        while( offset < dataDirectory.length() & Character.isSpaceChar( dataDirectory.charAt( offset)))
+        while( offset < dataDirectory.length() && Character.isSpaceChar( dataDirectory.charAt( offset)))
             offset ++;
         int leftParen = -1;
         int rightParen = -1;
@@ -99,7 +99,7 @@ public class JarStorageFactory extends BaseStorageFactory
         {
             jarFile = getJarFile( dataDirectory.substring( leftParen + 1, rightParen));
             offset = rightParen + 1;
-            while( offset < dataDirectory.length() & Character.isSpaceChar( dataDirectory.charAt( offset)))
+            while( offset < dataDirectory.length() && Character.isSpaceChar( dataDirectory.charAt( offset)))
                 offset ++;
             dataDirectory = dataDirectory.substring( offset, dataDirectory.length());
         }
