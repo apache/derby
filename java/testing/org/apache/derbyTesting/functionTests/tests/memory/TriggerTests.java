782dbe17fb11a9e1e2cf9f3aaa300baf3b6dd480
Kristian Waagan <kristwaa@apache.org>
2010-08-12 19:24:13 +0000

    DERBY-4764: Files with missing ASF license headers
    Added or modified Apache license headers where missing or old.
    
    Patch file: derby-4764-1a-license_headers_trunk.diff
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@984922 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/memory/TriggerTests.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/memory/TriggerTests.java
@@ -1,10 +1,26 @@
-/**
- * Repro for DERBY-1482:
- * Update triggers on tables with blob columns stream blobs
- * into memory even when the blobs are not referenced/accessed.
+/*
+
+   Derby - Class org.apache.derbyTesting.functionTests.tests.memory.TriggerTests
+
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
  */
 
 package org.apache.derbyTesting.functionTests.tests.memory;
+
 import java.sql.*;
 import java.util.Properties;
 
@@ -17,6 +33,11 @@ import org.apache.derbyTesting.junit.SystemPropertyTestSetup;
 import org.apache.derbyTesting.junit.TestConfiguration;
 import org.apache.derbyTesting.junit.JDBC;
 
+/**
+ * Repro for DERBY-1482:
+ * Update triggers on tables with blob columns stream blobs
+ * into memory even when the blobs are not referenced/accessed.
+ */
 public class TriggerTests extends BaseJDBCTestCase {
 
 	final int lobsize = 50000*1024;
