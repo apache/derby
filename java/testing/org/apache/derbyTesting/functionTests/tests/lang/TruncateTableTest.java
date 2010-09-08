8e47077f601ad24609ceff7ec8643eab63974f2f
Knut Anders Hatlen <kahatlen@apache.org>
2010-09-08 09:46:24 +0000

    DERBY-268: Test truncating tables with delete triggers and foreign keys
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@994946 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/lang/TruncateTableTest.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/lang/TruncateTableTest.java
@@ -21,6 +21,7 @@
 
 package org.apache.derbyTesting.functionTests.tests.lang;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -84,5 +85,73 @@ public class TruncateTableTest extends BaseJDBCTestCase {
                 };
         JDBC.assertFullResultSet(rs, expRS);
 
+    }
+
+    /**
+     * Test that TRUNCATE TABLE cannot be performed on a table with a
+     * delete trigger.
+     */
+    public void testTruncateWithDeleteTrigger() throws Exception {
+        Statement s = createStatement();
+
+        // Create two tables, t1 and t2, where deletes from t1 cause inserts
+        // into t2.
+        s.execute("create table deltriggertest_t1(x int)");
+        s.execute("create table deltriggertest_t2(y int)");
+        s.execute("create trigger deltriggertest_tr after delete on "
+                + "deltriggertest_t1 referencing old as old for each row "
+                + "insert into deltriggertest_t2 values old.x");
+
+        // Prepare a statement that checks the number of rows in the
+        // destination table (t2).
+        PreparedStatement checkDest = prepareStatement(
+                "select count(*) from deltriggertest_t2");
+
+        // Insert rows into t1, delete them, and verify that t2 has grown.
+        s.execute("insert into deltriggertest_t1 values 1,2,3");
+        JDBC.assertSingleValueResultSet(checkDest.executeQuery(), "0");
+        assertUpdateCount(s, 3, "delete from deltriggertest_t1");
+        JDBC.assertSingleValueResultSet(checkDest.executeQuery(), "3");
+
+        // Now do the same with TRUNCATE instead of DELETE. Expect it to fail
+        // because there is a delete trigger on the table.
+        s.execute("insert into deltriggertest_t1 values 4,5");
+        assertStatementError("XCL49", s, "truncate table deltriggertest_t1");
+        JDBC.assertSingleValueResultSet(checkDest.executeQuery(), "3");
+    }
+
+    /**
+     * Test that TRUNCATE TABLE isn't allowed on a table referenced by a
+     * foreign key constraint on another table.
+     */
+    public void testTruncateWithForeignKey() throws SQLException {
+        Statement s = createStatement();
+
+        // Create two tables with a foreign key relationship.
+        s.execute("create table foreignkey_t1(x int primary key)");
+        s.execute("create table foreignkey_t2(y int references foreignkey_t1)");
+        s.execute("insert into foreignkey_t1 values 1,2");
+        s.execute("insert into foreignkey_t2 values 2");
+
+        // Truncating the referenced table isn't allowed as that would
+        // break referential integrity.
+        assertStatementError("XCL48", s, "truncate table foreignkey_t1");
+
+        // Truncating the referencing table is OK.
+        s.execute("truncate table foreignkey_t2");
+        assertTableRowCount("FOREIGNKEY_T2", 0);
+    }
+
+    /**
+     * Test that TRUNCATE TABLE is allowed on a referenced table if it's only
+     * referenced by itself.
+     */
+    public void testSelfReferencing() throws SQLException {
+        Statement s = createStatement();
+        s.execute("create table self_referencing_t1(x int primary key, "
+                + "y int references self_referencing_t1)");
+        s.execute("insert into self_referencing_t1 values (1, null), (2, 1)");
+        s.execute("truncate table self_referencing_t1");
+        assertTableRowCount("SELF_REFERENCING_T1", 0);
     }
 }
