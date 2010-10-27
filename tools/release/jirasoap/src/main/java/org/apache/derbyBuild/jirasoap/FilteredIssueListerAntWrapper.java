1d8d20176345310e5001a12a075563d903fd3823
Kristian Waagan <kristwaa@apache.org>
2010-10-27 09:55:43 +0000

    DERBY-4857: Utilize the SOAP API to fetch JIRA issue list for release notes generation
    Integrated the JIRA SOAP tool with the ant genrelnotes target, which now:
     o checks if the required properties have been set
     o builds the JIRA SOAP tool using Maven 2
     o executes the JIRA SOAP tool to generate the issue list
     o executes the ReleaseNotesGenerator
    The target is started from the directory tools/release.
    Try 'ant -p' or see JavaDoc for ReleaseNotesGenerator for help on usage.
    
    Patch file: derby-4857-4b-jirasoap_ant_integration.diff
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1027882 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/release/jirasoap/src/main/java/org/apache/derbyBuild/jirasoap/FilteredIssueListerAntWrapper.java
@@ -0,0 +1,77 @@
+/*
+
+   Derby - Class org.apache.derbyBuild.jirasoap.FilteredIssueListerAntWrapper
+
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to you under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+
+package org.apache.derbyBuild.jirasoap;
+
+import org.apache.tools.ant.BuildException;
+
+/**
+ * Wrapper for invoking {@code FilteredIssueLister} from ant.
+ */
+public class FilteredIssueListerAntWrapper {
+
+    private String user;
+    private String password;
+    private String releaseVersion;
+    private long filterId;
+    private String output;
+
+    public FilteredIssueListerAntWrapper() {};
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setReleaseVersion(String releaseVersion) {
+        this.releaseVersion = releaseVersion;
+    }
+
+    public void setFilterId(String id)
+            throws BuildException {
+        try {
+            filterId = Long.parseLong(id);
+        } catch (NumberFormatException nfe) {
+            throw new BuildException(
+                    "invalid JIRA filter id (only digits allowed): " + id, nfe);
+        }
+    }
+
+    public void setOutputFile(String output) {
+        this.output = output;
+    }
+
+    public void execute()
+            throws BuildException {
+        try {
+            FilteredIssueLister issueLister =
+                new FilteredIssueLister(user, password);
+            issueLister.prepareReleaseNotes(
+                    releaseVersion, filterId, output, null);
+        } catch (Exception e) {
+            throw new BuildException(e);
+        }
+
+    }
+}
