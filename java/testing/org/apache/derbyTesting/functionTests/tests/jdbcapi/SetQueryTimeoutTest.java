8e4ceb175b74d4e780c287eac956fdbda88455b2
Richard N. Hillegas <rhillegas@apache.org>
2011-01-24 16:38:29 +0000

    Correct header of SetQueryTimeoutTest to reflect the actual package that the test lives in.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1062865 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/jdbcapi/SetQueryTimeoutTest.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/jdbcapi/SetQueryTimeoutTest.java
@@ -1,6 +1,6 @@
 /*
 
-   Derby - Class org.apache.derbyTesting.functionTests.tests.lang.SetQueryTimeoutTest
+   Derby - Class org.apache.derbyTesting.functionTests.tests.jdbcapi.SetQueryTimeoutTest
 
    Licensed to the Apache Software Foundation (ASF) under one or more
    contributor license agreements.  See the NOTICE file distributed with
