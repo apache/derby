75bf63db8caf1d9f55585f7cfcbf21fab91daf4e
Myrna van Lunteren <myrnavl@apache.org>
2011-02-17 15:21:46 +0000

    DERBY-4463; JMX test in nightly test suite failed with: JMXTest:clientjava.lang.InterruptedException   Causing the LockInterruptTest to be skipped with ibm 1.5 jvm unil SR13 is   available; this test caused a jvm bug in the handling of waitFor() to pop up.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1071640 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/engine/LockInterruptTest.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/engine/LockInterruptTest.java
@@ -25,6 +25,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 import junit.framework.Test;
+import junit.framework.TestSuite;
+
 import org.apache.derbyTesting.junit.BaseJDBCTestCase;
 import org.apache.derbyTesting.junit.CleanDatabaseTestSetup;
 import org.apache.derbyTesting.junit.DatabasePropertyTestSetup;
@@ -51,6 +53,18 @@ public class LockInterruptTest extends BaseJDBCTestCase {
     }
 
     public static Test suite() {
+        
+        if (! isSunJVM()) {
+            // DERBY-4463 test fails on IBM VM 1.5.
+            // It's fixed in IBM VM 1.6 SR9 and above.
+            // Remove this condition when that issue is solved in IBM VM 1.5 SR13.
+            if (getSystemProperty("java.version").startsWith("1.5.0"))
+            {
+                println("Test skipped for this VM, cf. DERBY-4463");
+                return new TestSuite("empty LockInterruptTest");
+            }
+        }
+        
         // Only run in embedded mode since we cannot interrupt the engine
         // thread from the network client.
         Test test = TestConfiguration.embeddedSuite(LockInterruptTest.class);
