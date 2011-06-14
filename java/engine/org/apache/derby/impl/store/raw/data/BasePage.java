b9130b74ff24a60de8fd2192c6b4a2a0ba99e104
Mike Matrigali <mikem@apache.org>
2011-06-14 17:56:11 +0000

    DERBY-5258 btree post commit releases latch before committing/aborting purges, possibly allowing other operation on page
    backporting change 1132711 from trunk to 10.4 branch.
    
    Fixing row level btree postcommit reclaim space to hold latch until end
    of the internal transaction. Before this fix there was a very small window
    (a few instructions) between the release of the latch and the commit of the
    transaction where another transaction could access the page, insert rows,
    and if a crash happens cause the undo of the purges of the reclaim space work
    to fail.
    
    It is proposed that this is what caused DERBY-5248, but without a repro it
    can't be proved. 
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/branches/10.4@1135720 13f79535-47bb-0310-9956-ffa450edef68

--- java/engine/org/apache/derby/impl/store/raw/data/BasePage.java
+++ java/engine/org/apache/derby/impl/store/raw/data/BasePage.java
@@ -1383,10 +1383,11 @@ abstract class BasePage implements Page, Observer, TypedFormat
 	*/
 	public void unlatch() {
 		if (SanityManager.DEBUG) {
-			SanityManager.ASSERT(isLatched());
+			SanityManager.ASSERT(isLatched(), 
+                "unlatch() attempted on page that is not latched.");
 		}
 
-	   releaseExclusive();
+        releaseExclusive();
 	}
 
 	/**
@@ -1407,7 +1408,8 @@ abstract class BasePage implements Page, Observer, TypedFormat
 	/** @see Page#recordCount */
 	public final int recordCount() {
 		if (SanityManager.DEBUG) {
-			SanityManager.ASSERT(isLatched());
+			SanityManager.ASSERT(
+                isLatched(), "page not latched on call to recordCount()");
 		}
 
 		return recordCount;
@@ -1448,9 +1450,14 @@ abstract class BasePage implements Page, Observer, TypedFormat
 						delCount++;
 				}
 				if (delCount != deletedCount)
-					SanityManager.THROWASSERT("incorrect deleted row count.  Should be: "
-						+ delCount + ", instead got: " + deletedCount
-						+ ", maxSlot = " + maxSlot + ", recordCount = " + recordCount);
+                {
+					SanityManager.THROWASSERT(
+                        "incorrect deleted row count.  Should be: " + delCount +
+                        ", instead got: " + deletedCount + 
+                        ", maxSlot = " + maxSlot + 
+                        ", recordCount = " + recordCount +
+                        "\npage = " + this);
+                }
 			}
 
 			return (recordCount - deletedCount);
