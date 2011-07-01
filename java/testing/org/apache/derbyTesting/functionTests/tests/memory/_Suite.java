d8931702e04ed1baf821f7d43b4ec95b04357174
Katherine Marsden <kmarsden@apache.org>
2011-07-01 12:54:30 +0000

    DERBY-3337 convert jdbcapi/derbyStress.java to JUnit DERBY-4200 client side OutOfMemoryError running derbnetclientmats:jdbcapi/derbyStress
    Convert test to JUnit and also run the finalizer periodically if freeMemory() falls below 500K.  The converted tests is no longer called derbyStress. It is now memory.MemoryLeakFixes and runs with the junit-lowmem target or can be run directly with junit.textui.TestRunner with -Xmx16M.  
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1141924 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/memory/_Suite.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/memory/_Suite.java
@@ -42,6 +42,7 @@ public class _Suite extends BaseJDBCTestCase {
         suite.addTest(RolesDependencyTest.suite());
         suite.addTest(Derby3009Test.suite());
         suite.addTest(XAMemTest.suite());
+        suite.addTest(MemoryLeakFixesTest.suite());
         return suite;
     }
 }
