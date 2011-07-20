bee1dd5321a84e5de34c4bad13eeccf0d29d55dc
Bryan Pendleton <bpendleton@apache.org>
2011-07-20 00:55:50 +0000

    DERBY-5332: Convert tools/ij3.sql to junit
    This patch was contributed by Houx Zhang (houxzhang at gmail dot com)
    
    This change adds a new test program, ij3Test.java, to the
    tools JUnit suite, and removes the old ij3.sql test program
    from the derbytools.runall suite.
    
    The purpose of the ij3Test program is to verify that ij can detect
    the driver information by examining the ij.database property at startup.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1148590 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/testing/org/apache/derbyTesting/functionTests/tests/tools/ij3Test.java
@@ -0,0 +1,50 @@
+/*
+
+   Derby - Class org.apache.derbyTesting.functionTests.tests.tools.ij3Test
+
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+
+package org.apache.derbyTesting.functionTests.tests.tools;
+
+import java.util.Properties;
+
+import junit.framework.Test;
+
+import org.apache.derbyTesting.functionTests.util.ScriptTestCase;
+import org.apache.derbyTesting.junit.CleanDatabaseTestSetup;
+import org.apache.derbyTesting.junit.SystemPropertyTestSetup;
+
+public class ij3Test extends ScriptTestCase {
+
+    public ij3Test(String script) {
+        super(script, true);
+    }
+    
+    public static Test suite() {        
+        Properties props = new Properties();
+        
+        props.setProperty("ij.database", "jdbc:derby:wombat;create=true");
+        props.setProperty("ij.showNoConnectionsAtStart", "true");
+        props.setProperty("ij.showNoCountForSelect", "true");
+        
+        Test test = new SystemPropertyTestSetup(new ij3Test("ij3"), props);
+        test = new CleanDatabaseTestSetup(test);   
+        
+        return getIJConfig(test); 
+    }   
+}
