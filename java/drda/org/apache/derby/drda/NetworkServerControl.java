273908908256174c310b97d4ae888e514c2fc817
Dag H. Wanvik <dag@apache.org>
2011-09-29 16:33:32 +0000

    DERBY-5413 NetworkServerControl#main can exit with status 1 without printing an error message to console
    Patch DERBY-5413, which makes sure the server doesn't die silently
    even if the debug flag has not been set. 
    
    Backported cleanly as "svn merge -c 1177324 <trunkrepo>"
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/branches/10.8@1177352 13f79535-47bb-0310-9956-ffa450edef68

--- java/drda/org/apache/derby/drda/NetworkServerControl.java
+++ java/drda/org/apache/derby/drda/NetworkServerControl.java
@@ -328,7 +328,7 @@ public class NetworkServerControl{
 			)
 			{
 				if (server != null)
-					server.consoleExceptionPrint(e);
+					server.consoleExceptionPrintTrace(e);
 				else
 					e.printStackTrace();  // default output stream is System.out
 			}
