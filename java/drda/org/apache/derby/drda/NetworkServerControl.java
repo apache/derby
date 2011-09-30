b1a53a8f444850576c4992340c254bb3d3ccd257
Dag H. Wanvik <dag@apache.org>
2011-09-30 02:23:36 +0000

    DERBY-5413 NetworkServerControl#main can exit with status 1 without printing an error message to console
    Undo the backported patch for this issue, turned out to be mistaken,
    cf explanation on issue.  It led to unwanted protocol output in some
    cases.
    
    svn merge -r 1177352:1177351  https://svn.apache.org/repos/asf/db/derby/code/branches/10.8
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/branches/10.8@1177483 13f79535-47bb-0310-9956-ffa450edef68

--- java/drda/org/apache/derby/drda/NetworkServerControl.java
+++ java/drda/org/apache/derby/drda/NetworkServerControl.java
@@ -328,7 +328,7 @@ public class NetworkServerControl{
 			)
 			{
 				if (server != null)
-					server.consoleExceptionPrintTrace(e);
+					server.consoleExceptionPrint(e);
 				else
 					e.printStackTrace();  // default output stream is System.out
 			}
