5dcbe31ee0f7bc09e0e48a6e36b81beeffc70e26
Knut Anders Hatlen <kahatlen@apache.org>
2011-10-10 10:05:19 +0000

    DERBY-5452: Possible NPE in NsTest.printException()
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1180858 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/system/nstest/NsTest.java
+++ java/testing/org/apache/derbyTesting/system/nstest/NsTest.java
@@ -606,7 +606,7 @@ public class NsTest extends Thread {
 						+ " SQLSTATE: " + m);
 			}
 		}
-		if (e.getMessage().equals(null)) {
+		if (e.getMessage() == null) {
 			System.out.println("NULL error message detected");
 			System.out.println("Here is the NULL exection - " + e.toString());
 			System.out.println("Stack trace of the NULL exception - ");
