dc97960deb9b818cea8f4753ca64a559b8a3929a
Knut Anders Hatlen <kahatlen@apache.org>
2011-10-24 15:46:52 +0000

    DERBY-5476: Remove unused parameter in splitmessages
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1188180 13f79535-47bb-0310-9956-ffa450edef68

--- java/build/org/apache/derbyBuild/splitmessages.java
+++ java/build/org/apache/derbyBuild/splitmessages.java
@@ -130,7 +130,6 @@ public class splitmessages {
 		File dir = new File(args[0]);
 
 		File source = new File(args[1]);
-        File clientDir = new File(args[2]);
         
 		String s = source.getName();
 		// loose the suffix
