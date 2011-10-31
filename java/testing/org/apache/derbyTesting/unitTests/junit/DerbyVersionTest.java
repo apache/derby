103248e39770915dac067e74d98dd4656de37ef1
Kristian Waagan <kristwaa@apache.org>
2011-10-31 14:40:38 +0000

    DERBY-5475: Formalize use of old Derby distributions in tests
    Fixed class name in license header.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1195481 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/unitTests/junit/DerbyVersionTest.java
+++ java/testing/org/apache/derbyTesting/unitTests/junit/DerbyVersionTest.java
@@ -1,6 +1,6 @@
 /*
 
-   Derby - Class org.apache.derbyTesting.unitTests.junit.InputStreamUtilTest
+   Derby - Class org.apache.derbyTesting.unitTests.junit.DerbyVersionTest
 
    Licensed to the Apache Software Foundation (ASF) under one or more
    contributor license agreements.  See the NOTICE file distributed with
