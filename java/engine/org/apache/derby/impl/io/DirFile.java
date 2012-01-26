9e26acc4550922c1f2232f366e3670297a454f4c
Mike Matrigali <mikem@apache.org>
2012-01-26 19:59:44 +0000

    DERBY-5574 encryption test in encryption nightly suite test fails with ERROR XBM0S: Unable to rename file error
    Catch errors on rename, and retry hoping that error is caused by some 
    temporary file system resource issue.  Similar methodology that other
    parts of the system uses on read and write errors.  Worst case system
    retries 5 times and still throws the error, best case a subsequent retry
    succeeds and user application is never aware an error was encountered.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1236370 13f79535-47bb-0310-9956-ffa450edef68

--- java/engine/org/apache/derby/impl/io/DirFile.java
+++ java/engine/org/apache/derby/impl/io/DirFile.java
@@ -39,6 +39,7 @@ import java.net.URL;
 import java.security.AccessControlException;
 import org.apache.derby.iapi.error.StandardException;
 import org.apache.derby.iapi.services.io.FileUtil;
+import org.apache.derby.iapi.util.InterruptStatus;
 import org.apache.derby.shared.common.reference.SQLState;
 
 /**
@@ -230,12 +231,16 @@ class DirFile extends File implements StorageFile
     } // end of getRandomAccessFile
 
     /**
-     * Rename the file denoted by this name. Note that StorageFile objects are immutable. This method
-     * renames the underlying file, it does not change this StorageFile object. The StorageFile object denotes the
-     * same name as before, however the exists() method will return false after the renameTo method
-     * executes successfully.
+     * Rename the file denoted by this name. 
      *
-     *<p>It is not specified whether this method will succeed if a file already exists under the new name.
+     * Note that StorageFile objects are immutable. This method renames the 
+     * underlying file, it does not change this StorageFile object. The 
+     * StorageFile object denotes the same name as before, however the exists()
+     * method will return false after the renameTo method executes successfully.
+     *
+     * <p>
+     * It is not specified whether this method will succeed if a file 
+     * already exists under the new name.
      *
      * @param newName the new name.
      *
@@ -243,7 +248,30 @@ class DirFile extends File implements StorageFile
      */
     public boolean renameTo( StorageFile newName)
     {
-        return super.renameTo( (File) newName);
+        boolean rename_status = super.renameTo( (File) newName);
+        int     retry_count   = 1;
+
+        while (!rename_status && (retry_count <= 5))
+        {
+            // retry operation, hoping a temporary I/O resource issue is 
+            // causing the failure.
+
+            try
+            {
+                Thread.sleep(1000 * retry_count);
+            }
+            catch (InterruptedException ie)
+            {
+                // This thread received an interrupt as well, make a note.
+                InterruptStatus.setInterrupted();
+            }
+
+            rename_status = super.renameTo((File) newName);
+
+            retry_count++;
+        }
+
+        return(rename_status);
     }
 
     /**
