401072ff096573fdf8de953c0597af7c6a3d284a
Dag H. Wanvik <dag@apache.org>
2013-01-14 23:39:56 +0000

    Fixed a comment typo.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1433218 13f79535-47bb-0310-9956-ffa450edef68

--- java/engine/org/apache/derby/iapi/reference/Attribute.java
+++ java/engine/org/apache/derby/iapi/reference/Attribute.java
@@ -325,7 +325,7 @@ public interface Attribute {
 		specified on the JDBC url at the database create time. This collation 
 		is what Derby 10.2 and prior have supported)
 		2)TERRITORY_BASED (the collation will be based on language 
-		region specified by the exisiting Derby attribute called territory. 
+       region specified by the existing Derby attribute called territory.
 		If the territory attribute is not specified at the database create 
 		time, Derby will use java.util.Locale.getDefault to determine the 
 		territory for the newly created database. 
