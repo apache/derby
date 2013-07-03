93fd82e115124755aa598feee896a231089fa603
Dyre Tjeldvoll <dyre@apache.org>
2013-07-03 09:20:47 +0000

    DERBY-5979: Part a: Do not use \n to terminate lines in Apache License header String constant and instead join the line strings with the value of line.separator property.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1499287 13f79535-47bb-0310-9956-ffa450edef68

--- java/build/org/apache/derbyPreBuild/ReleaseProperties.java
+++ java/build/org/apache/derbyPreBuild/ReleaseProperties.java
@@ -56,23 +56,22 @@ public class ReleaseProperties extends Task
     //  CONSTANTS
     //
     /////////////////////////////////////////////////////////////////////////
-
+    private static final String LS = System.getProperty("line.separator");
     private static final String APACHE_LICENSE_HEADER =
-        "# Licensed to the Apache Software Foundation (ASF) under one or more\n" +
-        "# contributor license agreements.  See the NOTICE file distributed with\n" +
-        "# this work for additional information regarding copyright ownership.\n" +
-        "# The ASF licenses this file to you under the Apache License, Version 2.0\n" +
-        "# (the \"License\"); you may not use this file except in compliance with\n" +
-        "# the License.  You may obtain a copy of the License at\n" +
-        "#\n" +
-        "#     http://www.apache.org/licenses/LICENSE-2.0\n" +
-        "#\n" +
-        "# Unless required by applicable law or agreed to in writing, software\n" +
-        "# distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-        "# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-        "# See the License for the specific language governing permissions and\n" +
-        "# limitations under the License.\n" +
-        "\n";
+        "# Licensed to the Apache Software Foundation (ASF) under one or more" + LS +
+        "# contributor license agreements.  See the NOTICE file distributed with" + LS +
+        "# this work for additional information regarding copyright ownership." + LS +
+        "# The ASF licenses this file to you under the Apache License, Version 2.0" + LS +
+        "# (the \"License\"); you may not use this file except in compliance with" + LS +
+        "# the License.  You may obtain a copy of the License at" + LS +
+        "#" + LS +
+        "#     http://www.apache.org/licenses/LICENSE-2.0" + LS +
+        "#" + LS +
+        "# Unless required by applicable law or agreed to in writing, software" + LS +
+        "# distributed under the License is distributed on an \"AS IS\" BASIS," + LS +
+        "# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied." + LS +
+        "# See the License for the specific language governing permissions and" + LS +
+        "# limitations under the License." + LS + LS;
     
 	public final static int	MAINT_ENCODING = 1000000;
     private final static int MAINT_LENGTH = 7;
