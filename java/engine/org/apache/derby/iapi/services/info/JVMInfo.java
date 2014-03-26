c40b7edf6ceda44519c8a2fde3442a2c50446eb6
Knut Anders Hatlen <kahatlen@apache.org>
2014-03-26 10:09:20 +0000

    DERBY-6518: JVMInfo should not use parseFloat() to parse java.specification.version
    Change the parse logic so that it recognizes that version 1.10 is greater than 1.9.
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1581777 13f79535-47bb-0310-9956-ffa450edef68

--- java/engine/org/apache/derby/iapi/services/info/JVMInfo.java
+++ java/engine/org/apache/derby/iapi/services/info/JVMInfo.java
@@ -120,8 +120,20 @@ public abstract class JVMInfo
 
             try {
 
-                if (Float.parseFloat(javaVersion) > 1.8f)
+                // Extract major and minor version out of the spec version.
+                String[] ver = javaVersion.split("[.]");
+                int major = ver.length >= 1 ? Integer.parseInt(ver[0]) : 0;
+                int minor = ver.length >= 2 ? Integer.parseInt(ver[1]) : 0;
+
+                // The highest level currently supported is 1.8. Use that
+                // level if the spec version is greater than or equal to it.
+                final int highestMajor = 1;
+                final int highestMinor = 8;
+                if (major > highestMajor ||
+                        (major == highestMajor && minor >= highestMinor)) {
                     id = J2SE_18;
+                }
+
             } catch (NumberFormatException nfe) {
             }
         }
