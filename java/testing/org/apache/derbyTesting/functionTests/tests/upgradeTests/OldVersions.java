cfa139d4aefca31ff636f9b3829c8d467dde48b0
Myrna van Lunteren <myrnavl@apache.org>
2014-04-16 02:10:02 +0000

    DERBY-6218; Tasks for producing a 10.10.2 maintenance release.   adding 10.10.2.0 to OldVersions.java
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1587780 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/tests/upgradeTests/OldVersions.java
+++ java/testing/org/apache/derbyTesting/functionTests/tests/upgradeTests/OldVersions.java
@@ -58,6 +58,7 @@ public class OldVersions
         {10, 8, 3, 0}, // 10.8.3.0 (November 16, 2012/ SVN 1405108)
         {10, 9, 1, 0}, // 10.9.1.0 (June 25, 2012/ SVN 1344872)
         {10, 10, 1, 1}, // 10.10.1.1 (April 15, 2013/ SVN 1458268)
+        {10, 10, 2, 0}, // 10.10.2.0 (April 15, 2014/ SVN 1582446)
     };
 
     //Constant for special upgrade testing with both upgrade and create 
