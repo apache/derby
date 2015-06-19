12a36413fedc75e161bf5173c36f0e68cdeb0b1b
Bryan Pendleton <bpendleton@apache.org>
2015-06-19 02:42:14 +0000

    DERBY-6810: Add regression tests for XXE vulnerability
    The new tests in XMLXXETest are reliable on Windows, but not on Unix. For the
    time being, until I can figure out where the test portability problem is,
    this change backs out revision 1686138.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1686347 13f79535-47bb-0310-9956-ffa450edef68

--- java/testing/org/apache/derbyTesting/functionTests/suites/XMLSuite.java
+++ java/testing/org/apache/derbyTesting/functionTests/suites/XMLSuite.java
@@ -50,7 +50,6 @@ public final class XMLSuite extends BaseTestCase {
         suite.addTest(org.apache.derbyTesting.functionTests.tests.lang.XMLBindingTest.suite());
         suite.addTest(org.apache.derbyTesting.functionTests.tests.lang.XMLMissingClassesTest.suite());
         suite.addTest(org.apache.derbyTesting.functionTests.tests.lang.XMLConcurrencyTest.suite());
-        suite.addTest(org.apache.derbyTesting.functionTests.tests.lang.XMLXXETest.suite());
         
         return suite;
     }
