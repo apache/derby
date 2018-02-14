e3f263c96ff67393b413b2de71b6c1c2a8d3ba58
Richard N. Hillegas <rhillegas@apache.org>
2018-02-14 01:50:58 +0000

    DERBY-6945: Remove obsolete classlister program; commit derby-6945-20-aa-removeClasslister.diff.
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/trunk@1824192 13f79535-47bb-0310-9956-ffa450edef68

--- java/build/org/apache/derbyBuild/maintversion2props.java
+++ java/build/org/apache/derbyBuild/maintversion2props.java
@@ -1,6 +1,6 @@
 /*
 
-   Derby - Class org.apache.derbyBuild.classlister
+   Derby - Class org.apache.derbyBuild.maintversion2props
 
    Licensed to the Apache Software Foundation (ASF) under one or more
    contributor license agreements.  See the NOTICE file distributed with
